eff46d7e59f4e7280595f52d7323b0ce8527612d
GeovaniSS <geosilvasousa@gmail.com>
2022-10-18 23:49:58 -0300

    ✨ feat: Input component

---
+++ src/components/Forms/Input.tsx
@@ -0,0 +1,24 @@
+import React, { InputHTMLAttributes } from 'react'
+import styles from './Input.module.css'
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  id: string
+  label: string
+  error?: string | null
+}
+
+export const Input = ({ id, label, error, ...props }: InputProps) => {
+  return (
+    <div className={styles.wrapper}>
+      <label htmlFor={id} className={styles.label}>{label}</label>
+      <input 
+        type="text" 
+        id={id} 
+        name={id}
+        className={styles.input}
+        {...props} 
+      />
+      { error && <small className={styles.error}>{error}</small> }
+    </div>
+  )
+}
