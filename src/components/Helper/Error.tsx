72a7a859b63d5369798d2a1ea875dd9c00a0154d
GeovaniSS <geosilvasousa@gmail.com>
2022-10-20 23:51:12 -0300

    ✨ feat: Error component

---
+++ src/components/Helper/Error.tsx
@@ -0,0 +1,12 @@
+interface ErrorProps {
+  error: string
+}
+
+export const Error = ({ error }: ErrorProps) => {
+  if (!error) return null 
+  return (
+    <p style={{ color: '#f31', margin: '1rem 0' }}>
+      {error}
+    </p>
+  )
+}
