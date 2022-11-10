b184a4da22ffb04af6d8902847496d8910d58fa5
GeovaniSS <geosilvasousa@gmail.com>
2022-11-09 22:15:52 -0300

    ✨ (Head, components): Add Head component | Used for change title and meta description

---
+++ src/components/Helper/Head.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface HeadProps {
+  title: string
+  description?: string
+}
+
+export const Head = ({ title, description = "" }: HeadProps) => {
+  React.useEffect(() => {
+    document.title = `${title} | Dogs`
+    document
+      .querySelector('meta[name="description"]')
+      ?.setAttribute('content', description)
+  }, []);
+
+  return <></>
+};
