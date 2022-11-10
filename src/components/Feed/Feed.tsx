59ef32cdfa13c6f4417a9d94a9c71a304c142716
GeovaniSS <geosilvasousa@gmail.com>
2022-11-09 22:33:19 -0300

    ✨ (Feed & FeedPhotos, components): Implements feed infinite scroll

--- src/components/Feed/Feed.tsx
+++ src/components/Feed/Feed.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import { Photo } from '../../api'
 
+import { Photo } from '../../api'
 import { FeedModal } from './FeedModal'
 import { FeedPhotos } from './FeedPhotos'
 
-export const Feed = () => {
+interface FeedProps {
+  user?: number | string
+}
+
+export const Feed = ({ user = 0 }: FeedProps) => {
+  const [pages, setPages] = React.useState<number[]>([1])
   const [modalPhoto, setModalPhoto] = React.useState<Photo | null>(null)
+  const [infinite, setInfinite] = React.useState(true)
 
   function selectPhoto(photo: Photo) {
     setModalPhoto(photo)
@@ -15,10 +21,48 @@ export const Feed = () => {
     setModalPhoto(null)
   }
 
+  function stopInfiniteScroll() {
+    setInfinite(false)
+  }
+
+  React.useEffect(() => {
+    let wait = false
+
+    function infiniteScroll() {
+      if (infinite) {
+        const scroll = window.scrollY
+        const height = document.body.offsetHeight - window.innerHeight
+        const isFeedBottomAlmostReached = scroll > 0.75 * height
+        
+        if (isFeedBottomAlmostReached && !wait) {
+          setPages(pages => [...pages, pages.length + 1])
+          wait = true
+          setTimeout(() => wait = false, 500)
+        }
+      }
+    }
+
+    window.addEventListener('scroll', infiniteScroll)
+    window.addEventListener('whell', infiniteScroll)
+
+    return () => {
+      window.removeEventListener('scroll', infiniteScroll)
+      window.removeEventListener('whell', infiniteScroll)
+    }
+  }, [infinite])
+
   return (
     <>
-      { modalPhoto && <FeedModal photo={modalPhoto} onCloseModal={closeModal} /> }
-      <FeedPhotos onSelectPhoto={selectPhoto}/>
+      { modalPhoto && <FeedModal id={modalPhoto.id} onCloseModal={closeModal} /> }
+      { pages.map(page => (
+        <FeedPhotos 
+          key={page} 
+          page={page} 
+          user={user} 
+          onSelectPhoto={selectPhoto}
+          onStopInfiniteScroll={stopInfiniteScroll}
+        />
+      ))}
     </>
   )
 }
--- src/components/Feed/FeedPhotos.tsx
+++ src/components/Feed/FeedPhotos.tsx
@@ -3,25 +3,31 @@ import styles from './FeedPhotos.module.css'
 
 import { useAxios } from '../../hooks/useAxios'
 import { Photo, PHOTOS_GET } from '../../api'
-import { FeedPhotoItem } from './FeedPhotoItem'
+
 import { Error } from '../Helper/Error'
 import { Loading } from '../Helper/Loading'
+import { FeedPhotoItem } from './FeedPhotoItem'
 
 interface FeedPhotosProps {
+  page: number
+  user: number | string
   onSelectPhoto: (photo: Photo) => void 
+  onStopInfiniteScroll: () => void 
 }
 
-export const FeedPhotos = ({ onSelectPhoto }: FeedPhotosProps) => {
+export const FeedPhotos = ({ page, user, onSelectPhoto, onStopInfiniteScroll }: FeedPhotosProps) => {
   const { data, error, loading, request } = useAxios()
 
-  async function fetchPhotos() {
-    const { url, options } = PHOTOS_GET({ total: 6, page: 1, user: 0 })
-    await request(url, options)
-  }
-
   React.useEffect(() => {
+    async function fetchPhotos() {
+      const total = 6
+      const { url, options } = PHOTOS_GET({ total, page: page, user: user})
+      const { response, data} = await request(url, options)
+      if (response.status && data.length < total) onStopInfiniteScroll()
+    }
+
     fetchPhotos()
-  }, [])
+  }, [user, page])
    
   if (error) return <Error error={error} />
   if (loading) return <Loading />
